94eaaded782475675cd55f60ae8b7a887a5fe8cb
Gadoli <gadoli.dodo@hotmail.fr>
2018-04-26 14:43:13 +0200

    Update Systeme.java
    Corrections des commentaires

--- src/Systeme.java
+++ src/Systeme.java
@@ -9,10 +9,10 @@ public abstract class Systeme {
 }
 
 /*
-* Le systeme Galaxy est considere comme le plus grand universe où l'on joue la simulation.
-* Le systeme Galaxy peut comprendre des systemes Solaires, par exemple s1 et s2,
-* mais pour etre efficace et reduire le temps inutile en realite, les planetes dans differants systeme, par exemple, p1 dans s1 et p2 dans s2,
-* on ne calcule pas l'influence entre les deux, car c'est tres petit dans presque tous les cas.
-* Par contre, on calcule chaque fois apres le mouvement de tous les planetes, si, par exemple, p1 est plus proche a la coeur de s1 ou celui de s2,
-* On toujours respecte le regle ou le planete est procede par le systeme le plus proche.    //  23/04/2018
+* Le système Galaxy est considéré comme le plus grand univers où l'on joue la simulation.
+* Le système Galaxy peut comprendre des systèmes planétaires, par exemple s1 et s2,
+* mais pour être efficace et réduire le temps inutile dans la réalite, les planètes dans différents système, par exemple, p1 dans s1 et p2 dans s2,
+* on ne calcule pas l'influence entre les deux, car c'est très petit dans pratiquiment tous les cas.
+* Par contre, on calcule à chaque fois après le mouvement de toutes les planètes, si, par exemple, p1 est plus proche du coeur de s1 ou celui de s2,
+* On respecte toujours la règle où la planète est précèdé par le système le plus proche.    //  23/04/2018
 * */
