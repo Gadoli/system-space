4f293c23542ce6c23d99b995e2779b75e587c2c7
Gadoli <gadoli.dodo@hotmail.fr>
2018-04-26 16:26:37 +0200

    Update SystemePlanetaire.java
    Added method ajouteSysTer, variable Planetes : ArrayList<Planete>

--- src/SystemePlanetaire.java
+++ src/SystemePlanetaire.java
@@ -1,11 +1,17 @@
 public abstract class SystemePlanetaire extends Systeme{
     private Vecteur positionRelative;
     private Vecteur vitesseRelative;
+    private ArrayList<Planete> Planetes;
 
     public SystemePlanetaire(String nom,Vecteur positionRelative,Vecteur vitesseRelative){
         super(nom);
         this.positionRelative=positionRelative;
         this.vitesseRelative=vitesseRelative;
+        Planetes = new ArrayList<Planete>();
+    }
+    
+    public void ajouteSysTer( SystemeTerrestre st){
+        Planetes.add(st);
     }
 
 }
