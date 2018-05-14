0af2d638d6d468270b7f9beeb3e986c554be6742
Gadoli <gadoli.dodo@hotmail.fr>
2018-05-14 09:26:28 +0000

    Update Planete.java
    Added toString method

--- src/Planete.java
+++ src/Planete.java
@@ -3,4 +3,8 @@ public class Planete extends Astre {
     public Planete(String nom,double masse,double rayonReel,Vecteur positionRelative,Vecteur vitesseRelative){
         super(nom,masse,rayonReel,positionRelative,vitesseRelative);
     }
+    
+    public String toString(){
+        return "o";
+    }
 }
