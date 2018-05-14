7f5550eb4093862ebb1a2e656df67f42bd71b033
Gadoli <gadoli.dodo@hotmail.fr>
2018-05-14 09:52:17 +0000

    Update and rename SystemeTerrestre.java to SystemeSatelitteNaturel.java
    Changing names for a better understanding

--- src/SystemeSatelitteNaturel.java
+++ src/SystemeSatelitteNaturel.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
-public class SystemeTerrestre extends Systeme{
+public class SystemeSatelitteNaturel extends Systeme{
     private Planete planete;
     private ArrayList<Sateille> satelittes;
     private Vecteur positionRelative;
     private Vecteur vitesseRelative;
 
-    public SystemeTerrestre(String nom,Planete planete,Vecteur positionRelative,Vecteur vitesseRelative){
+    public SystemeSatelitteNaturel(String nom,Planete planete,Vecteur positionRelative,Vecteur vitesseRelative){
         super(nom);
         this.positionRelative=positionRelative;
         this.vitesseRelative=vitesseRelative;
@@ -16,4 +16,6 @@ public class SystemeTerrestre extends Systeme{
     public void ajouteSatellite(Satellite s){
         satelittes.add(s);
     }
+    
+    
 }
