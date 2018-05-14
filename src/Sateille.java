39a5378741d3c57f5bf8d5e0b5f35262945ad225
Gadoli <gadoli.dodo@hotmail.fr>
2018-05-14 09:33:34 +0000

    Update Sateille.java
    Added toString & getNom method

--- src/Sateille.java
+++ src/Sateille.java
@@ -3,4 +3,12 @@ public class Sateille extends Astre {
     public Sateille(String nom,double masse,double rayonReel,Vecteur positionRelative,Vecteur vitesseRelative){
         super(nom,masse,rayonReel,positionRelative,vitesseRelative);
     }
+    
+    public String getNom(){
+        return this.nom;
+    }
+    
+    public String toString(){
+        return "°";
+    }
 }
