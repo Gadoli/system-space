5e567b1f931932c9700b336e83d04b1fb9321473
Gadoli <gadoli.dodo@hotmail.fr>
2018-05-23 14:20:14 +0000

    Delete UniversTest.java

--- UniversTest.java
+++
@@ -1,9 +0,0 @@
-//Work In Progress
-
-public class UniversTest{
-  public static void main(String[] args){
-    SystemeGalaxy voielactee = new SystemeGalaxy("Voie Lactee");
-    Vecteur v0 = new Vecteur(0,0,0);
-    Etoile s = new Etoile("Soleil",1989100000000000000000000000000,696342,v0,V0);
-  }
-}
